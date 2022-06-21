53c16386250509269edc756d667c009e04bd80f8
chayeongha <ckdudgk123@naver.com>
2022-06-21 10:37:56 +0900

    Part2 React 라이브러리 03~04 dayjs and date-fns

---
+++ date2/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+//import DayjsExample from './component/Dayjs/DayjsExample';
+import DateFnsExample from './component/DateFns/DateFnsExample';
+function App() {
+  return (
+    <div className="App">
+      {/* <DayjsExample/> */}
+      <DateFnsExample />
+    </div>
+  );
+}
+
+export default App;
